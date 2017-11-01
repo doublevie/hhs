ad2509abdb71518c100ba86c7dcd69ffcb47efd3
Fares ABADOU <fares.abadou@gmail.com>
2017-11-01 19:23:14 +0100

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Hotel el hidhab</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/froala/css/froala_blocks.css">
+    <link rel="stylesheet" href="assets/bootstrap4/css/bootstrap.min.css">
+  </head>
+  <body>
+
+
+<?php
+include "inc/header.php";
+ ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/jquery/dist/jquery.slim.min.js" ></script>
+    <script src="assets/bootstrap4/js/popper.min.js" ></script>
+    <script src="assets/bootstrap4/js/bootstrap.min.js" ></script>
+  </body>
+</html>
